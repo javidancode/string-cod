e5cbaee98a18155409e7e42b5a61bd826b1dd9c8
javidancode <javidanig@code.edu.az>
2022-06-22 02:44:43 +0400

    Added string cod table

---
+++ string cod/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace string_cod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int a = 5;
+            //int b = a;
+
+            //Console.WriteLine(a + "-" +b);
+
+
+
+            //int[] arr1 = { 1, 2, 7, 5, 9, 4 };
+            //int[] arr2 = arr1;
+            //arr2[0] = 100;
+
+            //Console.WriteLine(arr1[0]);
+            //Console.WriteLine(arr2[0]);
+
+
+            //int a = 6;
+            //CheckNum(a);
+            //Console.WriteLine(a);
+
+
+            //string name1 = "Cavidan";
+            //string name2 = name1;
+            //name2 = "coni";
+
+            //Console.WriteLine(name1 + "-" +name2);
+
+            //string name1 = "cavid";
+            //string name2 = "an";
+            //string fullname = name1 + name2;
+
+            //Console.WriteLine(fullname);
+
+
+
+            //stringBuilder name = new stringBuilder();
+
+            //name.Append("cavid");
+            //name.Append("an");
+
+            //Console.WriteLine(name);
+
+
+            string name = "Cavidan";
+
+            //name = string.Empty;
+
+            //if (string.IsNullOrEmpty(name))
+            //{
+            //    Console.WriteLine("dogrudur");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("sehvdir");
+            //}
+
+
+
+            //if (name.StartsWith("C"))
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+
+            //if (name.EndsWith("n"))
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+
+            //Console.WriteLine(name.Length);
+
+            //Console.WriteLine(name.IndexOf("d"));
+
+            //Console.WriteLine(name.Replace("v","i"));
+
+            //Console.WriteLine(name.Substring(5));
+
+            //string sentence = "salam , necesen , hardasan";
+
+            //var result = sentence.Split(",");
+
+            //foreach (var item in result)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine(name.ToLower());
+
+            //Console.WriteLine(name.ToUpper());
+        }
+
+
+
+  //      static void CheckNum(int num)
+		//{
+  //          num += 10;
+
+  //          Console.WriteLine($"Number : {num}");
+  //      }
+    }
+}
